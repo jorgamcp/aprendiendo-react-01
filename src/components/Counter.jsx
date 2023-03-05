f28092bf74fe764c03cb3a99969159288827e925
jorge gamero <jorgam31@gmail.com>
2023-03-05 11:58:47 +0100

    Main CRUD Func Works only in client

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,19 +1,19 @@
 
-import {useState } from 'react';
+import { useState } from 'react';
 
-export default function Counter(){
+export default function Counter() {
 
-    const [counter,setCounter] = useState(0);
+    const [counter, setCounter] = useState(0);
 
     const updateCounter = () => {
-        setCounter(counter+1);
+        setCounter(counter + 1);
     }
 
     return (
         <div>
-           <h3>Counter Component</h3>
-           <p>Mi número de counter vale {counter}</p>
-           <button onClick={updateCounter}>Actualizar Contador</button>
+            <h3>Counter Component</h3>
+            <p>Mi número de counter vale {counter}</p>
+            <button onClick={updateCounter}>Actualizar Contador</button>
         </div>
     )
 }
